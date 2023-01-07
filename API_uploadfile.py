d6bc775a503cddb9318c759d6755f2f5b7df2e4b
atoub <anass.toub@gmail.com>
2023-01-07 03:13:29 +0100

    this will upload the the pdf and nomalise it

--- API_uploadfile.py
+++ API_uploadfile.py
@@ -5,9 +5,21 @@ from normalisation import extract_text_from_pdf, split_text_into_sentences_and_w
 
 app = FastAPI()
 
-@app.post("/upload-file/")
-async def upload_pdf(file: UploadFile = File(..., media_type="application/pdf")):
-    text = extract_text_from_pdf(file)
+@app.post("/pdf-text/")
+async def extract_pdf_text(pdf_file: UploadFile = File(..., media_type="application/pdf")):
+    # Get the file name and content
+    file_name = pdf_file.filename
+    file_content = await pdf_file.read()
+    # Save the PDF file
+    with open(file_name, 'wb') as f:
+        f.write(file_content)
+    # Extract the text from the PDF file
+    text = extract_text_from_pdf(file_name)
+    # split the text into sentences and words
     sentences = split_text_into_sentences_and_words(text)
-    sentences = remove_stop_words(sentences)
-    return {"sentences": sentences}
+    # Remove stop words from each sentence
+    filtered_sentences = remove_stop_words(sentences)
+    # Return the text
+    return {'filtred_Sentences': filtered_sentences}
+
+
